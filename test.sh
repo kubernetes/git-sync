7cc85f3c1b7a848d5470a65499ec8bea63d86474
Andrey Zhelnin <andjel@gmail.com>
2018-01-31 15:46:22 +0300

    Webhook call on sucessfull sync added

--- test.sh
+++
@@ -1,9 +0,0 @@
-docker run \
-    -v /tmp/git-data:/git \
-    registry/git-sync:v2.0.5-22-g3897ab3-dirty \
-        --repo=https://github.com/kubernetes/git-sync \
-        --dest=test \
-	--branch=master \
-        --wait=30 \
-        --webhook-url=http://localhost:80/test
-
