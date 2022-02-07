76854fc806b327c8e02c75c9f0e31f8820bf60fc
Tim Hockin <thockin@google.com>
2022-02-06 16:36:58 -0800

    Set $GITSYNC_HASH in exechook

--- pkg/cmd/cmd.go
+++ pkg/cmd/cmd.go
@@ -38,18 +38,21 @@ func NewRunner(logger *logging.Logger) *Runner {
 }
 
 // Run runs given command
-func (r *Runner) Run(ctx context.Context, cwd, command string, args ...string) (string, error) {
-	return r.RunWithStdin(ctx, cwd, "", command, args...)
+func (r *Runner) Run(ctx context.Context, cwd string, env []string, command string, args ...string) (string, error) {
+	return r.RunWithStdin(ctx, cwd, env, "", command, args...)
 }
 
 // RunWithStdin runs given command with stardart input
-func (r *Runner) RunWithStdin(ctx context.Context, cwd, stdin, command string, args ...string) (string, error) {
+func (r *Runner) RunWithStdin(ctx context.Context, cwd string, env []string, stdin, command string, args ...string) (string, error) {
 	cmdStr := cmdForLog(command, args...)
 	r.logger.V(5).Info("running command", "cwd", cwd, "cmd", cmdStr)
 
 	cmd := exec.CommandContext(ctx, command, args...)
 	if cwd != "" {
 		cmd.Dir = cwd
+	}
+	if len(env) != 0 {
+		cmd.Env = env
 	}
 	outbuf := bytes.NewBuffer(nil)
 	errbuf := bytes.NewBuffer(nil)
