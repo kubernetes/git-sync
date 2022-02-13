2538d293ba71ff1e100e9cb622660f778c062c88
Tim Hockin <thockin@google.com>
2022-02-12 17:38:36 -0800

    Set repoReady even when there wasn't a 1st clone
    E.g. if the repo is already present (after a restart).

--- cmd/git-sync/main.go
+++ cmd/git-sync/main.go
@@ -614,17 +614,20 @@ func main() {
 			cancel()
 			time.Sleep(*flPeriod)
 			continue
-		} else if changed {
-			if webhookRunner != nil {
-				webhookRunner.Send(hash)
-			}
-			if exechookRunner != nil {
-				exechookRunner.Send(hash)
-			}
-
-			updateSyncMetrics(metricKeySuccess, start)
 		} else {
-			updateSyncMetrics(metricKeyNoOp, start)
+			// this might have been called before, but also might not have
+			setRepoReady()
+			if changed {
+				if webhookRunner != nil {
+					webhookRunner.Send(hash)
+				}
+				if exechookRunner != nil {
+					exechookRunner.Send(hash)
+				}
+				updateSyncMetrics(metricKeySuccess, start)
+			} else {
+				updateSyncMetrics(metricKeyNoOp, start)
+			}
 		}
 
 		if initialSync {
