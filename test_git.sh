d12d9f2a01bec53f905a2dc72ef4a73f7fbf9628
Raúl Benencia <id@rbenencia.name>
2024-06-28 11:29:31 -0700

    test_git.sh: declare restore_opts and assign separately
    This avoids masking return values. Fixes SC2155. Part of #891.

--- test_git.sh
+++ test_git.sh
@@ -908,7 +908,8 @@ function run_test() {
     shift
 
     declare -g "$retvar"
-    local restore_opts=$(set +o)
+    local restore_opts
+    restore_opts=$(set +o)
     set +o errexit
     set +o nounset
     set +o pipefail
